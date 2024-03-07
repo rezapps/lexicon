64d6270425386fd80fdd98de21c4d17bb5b0cd3d
rjtechdev <yo-rez@outlook.com>
2024-03-07 12:51:04 +0100

    changed PersonHandler methods to static. Used classname to invoke PersonHandler methods in Program.cs

--- OOPBasics/Program.cs
+++ OOPBasics/Program.cs
@@ -4,12 +4,9 @@
 	{
 		static void Main(string[] args)
 		{
-			PersonHandler personHandler = new();
-
 			Person johnDoe = PersonHandler.CreatePerson(25, "John", "Doe", 1.80, 75.0);
 			PersonHandler.SetAge(johnDoe, 30);
 			Console.WriteLine($"Person 1: {johnDoe.FirstName} {johnDoe.LastName}, Age: {johnDoe.Age}");
-
 		}
 	}
 }
--- OOPBasics/obj/Debug/net8.0/OOPBasics.AssemblyInfo.cs
+++ OOPBasics/obj/Debug/net8.0/OOPBasics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPBasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a190ff6d57d034f32510eb76ec3ee74dd8c0d3e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f090890d5a1849a24ff476f6413e1d803a054d8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPBasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPBasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
